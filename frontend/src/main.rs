eac8c5d9c0d1931c63b7ef0dd93bdab1226a9f99
LDprg <71488985+LDprg@users.noreply.github.com>
2023-06-26 20:20:40 +0200

    more basic changes

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -10,10 +10,17 @@ fn App() -> Html {
         }
     };
 
+    let switch = {
+        move |_| {
+
+        }
+    };
+
     html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
+        <div class="flex">
+            <button onclick={onclick} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{ "+1" }</button>
             <p>{ *counter }</p>
+            <button onclick={switch} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{ "toggle" }</button>
         </div>
     }
 }
